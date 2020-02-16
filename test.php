eeec7413dbe46d40ddd5b887fc175b6900de9a45
shikamaru <n_Shikamaru@icloud.com>
2020-02-16 13:25:57 +0800

    PHP语法

--- test.php
+++ test.php
@@ -1,1 +1,20 @@
-<?php
+<html lang="">
+    <h1>
+        My first PHP page.
+    </h1>
+    <div>
+        <?php
+        echo "Hello PHP from [echo].";
+        ?>
+
+        <?php
+        //这是PHP单行注释
+        /*这是PHP
+        多行注释*/
+        print "Hello PHP from [print]."
+        ?>
+    </div>
+</html>
+
+<!--PHP 中的每个代码行都必须以分号结束。分号是一种分隔符，用于把指令集区分开来。-->
+<!--通过 PHP，有两种在浏览器输出文本的基础指令：echo 和 print。-->
